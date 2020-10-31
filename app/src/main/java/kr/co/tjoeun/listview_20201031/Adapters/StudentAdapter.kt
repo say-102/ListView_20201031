695cff9f242f1fc5474c6df74ccc24b31f14621f
say-102 <say70007@naver.com>
2020-10-31 14:15:02 +0900

    현재 시간을 받아와서, 해당 날짜 기준의 나이 계산  - Calender 클래스 사용 예시

--- app/src/main/java/kr/co/tjoeun/listview_20201031/Adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20201031/Adapters/StudentAdapter.kt
@@ -8,6 +8,9 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import kr.co.tjoeun.listview_20201031.R
 import kr.co.tjoeun.listview_20201031.datas.Student
+import java.time.Year
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StudentAdapter(
     val mContext:Context,
@@ -36,9 +39,11 @@ class StudentAdapter(
 
 //        생년을 가지고 그대로 찍으면 잘못된 데이터. => 나이로 변환 해야함. (알고리즘 고민)
 
-//        알고리즘 예시 : 생년을 가지고 -> (2020년 기준) 나이를 구하기.
+//        알고리즘 예시 : 생년을 가지고 -> (앱을 킨 시점 기준) 나이를 구하기.
 
-        val  age = 2020 - studentData.birthYear + 1
+//        현재시간의 일/시/분/초/년/월 등등 모두 저장됨.
+        val now = Calendar.getInstance()
+        val  age = now.get(Calendar.YEAR) - studentData.birthYear + 1
 
         ageTxt.text = "${age}세"
 
