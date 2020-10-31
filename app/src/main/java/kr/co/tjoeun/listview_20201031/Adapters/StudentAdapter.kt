9a0eccfba6764dbec53d2c7ae1c7f434727db1b9
say-102 <say70007@naver.com>
2020-10-31 12:30:59 +0900

    학생 목록을 뿌려주는 어댑터 기초 작업

---
+++ app/src/main/java/kr/co/tjoeun/listview_20201031/Adapters/StudentAdapter.kt
@@ -0,0 +1,31 @@
+package kr.co.tjoeun.listview_20201031.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.listview_20201031.R
+import kr.co.tjoeun.listview_20201031.datas.Student
+
+class StudentAdapter(
+    val mContext:Context,
+    val resld:Int,
+    val mList : ArrayAdapter<Student>) : ArrayAdapter<Student>(mContext, resld, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+        val row = tempRow! !
+        
+        return row
+    }
+
+
+    }
+
+}
