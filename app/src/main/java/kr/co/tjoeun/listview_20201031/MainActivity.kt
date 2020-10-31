5241b6f5d6b4aae7a3146389fcb2dc1ff0d06426
say-102 <say70007@naver.com>
2020-10-31 12:47:02 +0900

    학색목록

--- app/src/main/java/kr/co/tjoeun/listview_20201031/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20201031/MainActivity.kt
@@ -2,10 +2,21 @@ package kr.co.tjoeun.listview_20201031
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.listview_20201031.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("조경진", "서울시 은평구", 1988))
+        mStudentList.add(Student("구본아", "서울시 용산구", 1991))
+        mStudentList.add(Student("김하은", "서울시 구로구", 1995))
+        mStudentList.add(Student("박성윤", "인천시 연수구", 1996))
+        mStudentList.add(Student("조윤주", "서울시 구로구", 1996))
+
     }
 }
